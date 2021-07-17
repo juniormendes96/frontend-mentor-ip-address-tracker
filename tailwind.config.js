25e55263428bfe3081c310e178457a0428b60f11
Junior <vilmarmjrr@gmail.com>
2021-07-17 14:40:26 -0300

    setup tailwindcss and font-family

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Rubik', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  variants: {
+    extend: {}
+  },
+  plugins: []
+};
